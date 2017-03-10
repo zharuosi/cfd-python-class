f580875f7c8b13248fcf68594cb6654328ccc520
zharuosi <zharuosi@gmail.com>
2017-03-10 19:59:10 -0330

    Start the first lesson

---
+++ code/step1LinearConvection.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Solve the 1-D Linear convection equation:
+# $\frac{\partial u}{\partial t} + c \frac{\partial u}{\partial x} = 0$
+# Time derivative: Forward Difference scheme
+# Space derivative: Backward Difference scheme
+
